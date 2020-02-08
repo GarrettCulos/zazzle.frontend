88dbb66c4d3ea63a9b9f4c3d36eebc5017b19e8a
garrett <15315752+GarrettCulos@users.noreply.github.com>
2020-02-08 11:17:41 -0800

    Update prod build

--- config-overrides.js
+++ config-overrides.js
@@ -5,12 +5,16 @@ const resolve = dir => path.resolve(__dirname, dir);
 module.exports = {
   // The Webpack config to use when compiling your react app for development or production.
   webpack: function(config, env) {
-    console.log(env);
+    let configPath = resolve('src/environment/environment');
+    console.log(process.env.REACT_APP_CONFIG_ENV);
+    if (process.env.REACT_APP_CONFIG_ENV === 'production') {
+      configPath = resolve('src/environment/environment.prod');
+    }
     // ...add your webpack config
     config.resolve.alias = Object.assign(config.resolve.alias, {
       '@store': resolve('src/store'),
       '@services': resolve('src/services'),
-      '@environment': resolve('src/environment'),
+      '@environment/environment': configPath,
       '@hooks': resolve('src/hooks'),
       '@gql': resolve('src/apollo'),
       '@appTypes': resolve('src/types')
